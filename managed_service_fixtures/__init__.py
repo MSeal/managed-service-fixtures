88a4ca7caf6242137daad071011d5ea2a6b4e009
Kafonek <matt.kafonek@noteable.io>
2022-06-29 05:28:40 -0400

    move structure into src/ and fix errors preventing lib from being imported

--- managed_service_fixtures/__init__.py
+++
@@ -1,4 +0,0 @@
-from services.cockroach import CockroachDetails, managed_cockroach
-from services.moto import MotoDetails, managed_moto
-from services.redis import RedisDetails, managed_redis
-from services.vault import VaultDetails, managed_vault
